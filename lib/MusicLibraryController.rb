67d6c0969813d6326692b42e180abb0b10fcb9c6
Sonja Parsell <hello@sparsell.com>
2020-05-26 14:03:23 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -74,7 +74,7 @@ class MusicLibraryController
       if genre = Genre.find_by_name(input)
         genre.songs.sort {|a, b| a.name <=> b.name}.each_with_index {|song, index| puts "#{index+1}. #{genre.name} - #{song.genre.name}"}
 
-
+      end
   end
 
   def play_song
